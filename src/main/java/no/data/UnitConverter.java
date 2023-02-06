2ade8be18c0593cbd050275ac880972bb0df0724
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2023-02-06 11:49:16 -0600

    :test: setup junit 4 tests

---
+++ src/main/java/no/data/UnitConverter.java
@@ -0,0 +1,8 @@
+package no.data;
+
+public class UnitConverter {
+    public static double convertCelsiusToFahrenheit(double celsius){
+        double fahrenheit = celsius * 1.8 + 32;
+        return fahrenheit;
+    }
+}
---
+++ src/test/java/no/data/UnitConverterTest.java
@@ -0,0 +1,10 @@
+package no.data;
+
+import static org.junit.Assert.*;
+
+public class UnitConverterTest {
+
+    @org.junit.Test
+    public void convertCelsiusToFahrenheit() {
+    }
+}
