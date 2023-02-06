3ebed3c60c26f34e8697047defb5819715e80dc4
Victor Jose Arana Rodriguez <victorj.arana@gmail.com>
2023-02-06 11:52:43 -0600

    :test: succeed a couple of tests

--- src/test/java/no/data/UnitConverterTest.java
+++ src/test/java/no/data/UnitConverterTest.java
@@ -6,5 +6,15 @@ public class UnitConverterTest {
 
     @org.junit.Test
     public void convertCelsiusToFahrenheit() {
+        double input = 32.9;
+        double expectedOutput = 91.22;
+        assertTrue(expectedOutput == UnitConverter.convertCelsiusToFahrenheit(input));
+    }
+
+    @org.junit.Test
+    public void convertCelsiusToFahrenheit_02() {
+        double input = 0;
+        double expectedOutput = 32.0;
+        assertTrue(expectedOutput == UnitConverter.convertCelsiusToFahrenheit(input));
     }
 }
